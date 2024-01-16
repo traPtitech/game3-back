6aa1e56fc55e59a7077766e0aae4e2dacb52905a
pikachu0310 <pikachu13711@gmail.com>
2024-01-16 22:18:59 +0900

    :sparcles: NeoShowcaseで動くようにDBの設定

--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -23,15 +23,28 @@ func AppAddr() string {
 func MySQL() *mysql.Config {
 	c := mysql.NewConfig()
 
-	c.User = getEnv("DB_USER", "root")
-	c.Passwd = getEnv("DB_PASSWORD", "pass")
-	c.Net = getEnv("DB_NET", "tcp")
-	c.Addr = fmt.Sprintf(
-		"%s:%s",
-		getEnv("DB_HOST", "localhost"),
-		getEnv("DB_PORT", "3306"),
-	)
-	c.DBName = getEnv("DB_NAME", "app")
+	neoshowcase := getEnv("NEOSHOWCASE", "0")
+	if neoshowcase == "1" {
+		c.User = getEnv("NS_MARIADB_USER", "root")
+		c.Passwd = getEnv("NS_MARIADB_PASSWORD", "pass")
+		c.Net = getEnv("DB_NET", "tcp")
+		c.Addr = fmt.Sprintf(
+			"%s:%s",
+			getEnv("NS_MARIADB_HOSTNAME", "localhost"),
+			getEnv("NS_MARIADB_PORT", "3306"),
+		)
+		c.DBName = getEnv("NS_MARIADB_DATABASE", "app")
+	} else {
+		c.User = getEnv("DB_USER", "root")
+		c.Passwd = getEnv("DB_PASSWORD", "pass")
+		c.Net = getEnv("DB_NET", "tcp")
+		c.Addr = fmt.Sprintf(
+			"%s:%s",
+			getEnv("DB_HOST", "localhost"),
+			getEnv("DB_PORT", "3306"),
+		)
+		c.DBName = getEnv("DB_NAME", "app")
+	}
 	c.Collation = "utf8mb4_general_ci"
 	c.AllowNativePasswords = true
 	c.ParseTime = true
