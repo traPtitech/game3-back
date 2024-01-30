c26092cc787fdc880e8fed7a40e1798b6af4e6e5
pikachu0310 <pikachu13711@gmail.com>
2024-01-30 11:05:24 +0900

    :white_check_mark: DB Schemaの変更を反映

--- internal/migration/1_schema.sql
+++ internal/migration/1_schema.sql
@@ -1,17 +1,75 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE sessions
+CREATE TABLE session
 (
     id            CHAR(36) PRIMARY KEY,
+    discordUserId CHAR(36),
     redirect      VARCHAR(255) NOT NULL,
     access_token  VARCHAR(255),
     refresh_token VARCHAR(255),
+    expires_in    INT,
     created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    expires_in    INT
+    updated_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+-- +goose StatementEnd
+
+-- +goose StatementBegin
+CREATE TABLE event
+(
+    slug       VARCHAR(255) PRIMARY KEY,
+    title      VARCHAR(255) NOT NULL,
+    startAt    TIMESTAMP    NOT NULL,
+    endAt      TIMESTAMP    NOT NULL,
+    created_at TIMESTAMP    DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+-- +goose StatementEnd
+
+-- +goose StatementBegin
+CREATE TABLE term
+(
+    id         CHAR(36) PRIMARY KEY,
+    eventSlug  VARCHAR(255) NOT NULL,
+    isDefault  BOOLEAN NOT NULL,
+    startAt    TIMESTAMP NOT NULL,
+    endAt      TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (eventSlug) REFERENCES event(slug)
+);
+-- +goose StatementEnd
+
+-- +goose StatementBegin
+CREATE TABLE game
+(
+    id              CHAR(36) PRIMARY KEY,
+    termId          CHAR(36) NOT NULL,
+    discordUserId   CHAR(36) NOT NULL,
+    creatorName     VARCHAR(255) NOT NULL,
+    creatorPageUrl  VARCHAR(255),
+    gamePageUrl     VARCHAR(255),
+    title           VARCHAR(255) NOT NULL,
+    description     TEXT,
+    place           VARCHAR(255),
+    icon            VARCHAR(255),
+    image           VARCHAR(255),
+    created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (termId) REFERENCES term(id),
+    FOREIGN KEY (discordUserId) REFERENCES session(id)
 );
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE IF EXISTS sessions;
+DROP TABLE IF EXISTS session;
+-- +goose StatementEnd
+-- +goose StatementBegin
+DROP TABLE IF EXISTS game;
+-- +goose StatementEnd
+-- +goose StatementBegin
+DROP TABLE IF EXISTS term;
+-- +goose StatementEnd
+-- +goose StatementBegin
+DROP TABLE IF EXISTS event;
 -- +goose StatementEnd
