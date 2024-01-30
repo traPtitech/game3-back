abaacdeb41bab5fe30d99f4e155ee0b4e0fe523b
pikachu0310 <pikachu13711@gmail.com>
2024-01-30 21:36:36 +0900

    :white_check_mark: DB Schemaの変更をdomainsに反映

--- internal/domains/session.go
+++ internal/domains/session.go
@@ -5,6 +5,19 @@ import (
 	"time"
 )
 
+/*
+CREATE TABLE session
+(
+    id            CHAR(36) PRIMARY KEY,
+    redirect      VARCHAR(255) NOT NULL,
+    access_token  VARCHAR(255),
+    refresh_token VARCHAR(255),
+    expires_in    INT,
+    created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+*/
+
 type Session struct {
 	ID           *uuid.UUID `db:"id"`
 	Redirect     *string    `db:"redirect"`
@@ -12,4 +25,5 @@ type Session struct {
 	RefreshToken *string    `db:"refresh_token"`
 	ExpiresIn    *int       `db:"expires_in"`
 	CreatedAt    *time.Time `db:"created_at"`
+	UpdatedAt    *time.Time `db:"updated_at"`
 }
