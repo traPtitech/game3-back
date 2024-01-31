366be3cd20c3ea3276c5e6a008566ca20ae00c29
pikachu0310 <pikachu13711@gmail.com>
2024-01-31 15:20:53 +0900

    :memo: DBのSchemaをスネークケースに変更

--- internal/migration/1_schema.sql
+++ internal/migration/1_schema.sql
@@ -15,13 +15,13 @@ CREATE TABLE session
 -- +goose StatementBegin
 CREATE TABLE event
 (
-    slug       VARCHAR(255) PRIMARY KEY,
-    title      VARCHAR(255) NOT NULL,
-    startAt    TIMESTAMP    NOT NULL,
-    endAt      TIMESTAMP    NOT NULL,
-    image      LONGBLOB,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    slug                         VARCHAR(255) PRIMARY KEY,
+    title                        VARCHAR(255) NOT NULL,
+    game_submission_period_start TIMESTAMP    NOT NULL,
+    game_submission_period_end   TIMESTAMP    NOT NULL,
+    image                        LONGBLOB,
+    created_at                   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at                   TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 -- +goose StatementEnd
 
@@ -29,34 +29,33 @@ CREATE TABLE event
 CREATE TABLE term
 (
     id         CHAR(36) PRIMARY KEY,
-    eventSlug  VARCHAR(255) NOT NULL,
-    isDefault  BOOLEAN      NOT NULL,
-    startAt    TIMESTAMP    NOT NULL,
-    endAt      TIMESTAMP    NOT NULL,
+    event_slug VARCHAR(255) NOT NULL,
+    is_default BOOLEAN      NOT NULL,
+    start_at   TIMESTAMP    NOT NULL,
+    end_at     TIMESTAMP    NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (eventSlug) REFERENCES event (slug)
+    FOREIGN KEY (event_slug) REFERENCES event (slug)
 );
 -- +goose StatementEnd
 
 -- +goose StatementBegin
 CREATE TABLE game
 (
-    id             CHAR(36) PRIMARY KEY,
-    termId         CHAR(36)     NOT NULL,
-    discordUserId  CHAR(36)     NOT NULL,
-    creatorName    VARCHAR(255) NOT NULL,
-    creatorPageUrl VARCHAR(255),
-    gamePageUrl    VARCHAR(255),
-    title          VARCHAR(255) NOT NULL,
-    description    TEXT,
-    place          VARCHAR(255),
-    icon           MEDIUMBLOB   NOT NULL,
-    image          LONGBLOB,
-    created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (termId) REFERENCES term (id),
-    FOREIGN KEY (discordUserId) REFERENCES session (id)
+    id               CHAR(36) PRIMARY KEY,
+    term_id          CHAR(36)     NOT NULL,
+    discord_user_id  CHAR(36)     NOT NULL,
+    creator_name     VARCHAR(255) NOT NULL,
+    creator_page_url VARCHAR(255),
+    game_page_url    VARCHAR(255),
+    title            VARCHAR(255) NOT NULL,
+    description      TEXT,
+    place            VARCHAR(255),
+    icon             MEDIUMBLOB   NOT NULL,
+    image            LONGBLOB,
+    created_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (term_id) REFERENCES term (id)
 );
 -- +goose StatementEnd
 
