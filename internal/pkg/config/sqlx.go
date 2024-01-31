b275cca424d168e3979a7b739c3c189ebe69dab2
pikachu0310 <pikachu13711@gmail.com>
2024-01-31 15:21:34 +0900

    :sparcles: sqlxの機能でdbタグが存在しない構造体はスネークケースに変換して検索

---
+++ internal/pkg/config/sqlx.go
@@ -0,0 +1,20 @@
+package config
+
+import (
+	"github.com/jmoiron/sqlx"
+	"regexp"
+	"strings"
+)
+
+func init() {
+	sqlx.NameMapper = toSnakeCase
+}
+
+func toSnakeCase(str string) string {
+	var matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
+	var matchAllCap = regexp.MustCompile("([a-z0-9])([A-Z])")
+	snake := matchFirstCap.ReplaceAllString(str, "${1}_${2}")
+	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
+
+	return strings.ToLower(snake)
+}
